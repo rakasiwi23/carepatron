26ec7f130fc3bef73e57f39af8ad2251a9914deb
Andhika Prakasiwi <a.prakasiwi@gmail.com>
2023-10-20 20:46:24 +0700

    feat: setup react-query

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,18 +1,31 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import DataProvider from "./store/DataProvider";
-import Clients from "./pages/Clients";
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import DataProvider from './store/DataProvider';
+import Clients from './pages/Clients';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 export default function App() {
-  return (
-    <div className="App">
-      <DataProvider>
-        <Routes>
-          <Route path="/" element={<Clients />} />
-          <Route path="/Clients" element={<Clients />} />
-        </Routes>
-      </DataProvider>
-    </div>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<div className='App'>
+				<DataProvider>
+					<Routes>
+						<Route path='/' element={<Clients />} />
+						<Route path='/Clients' element={<Clients />} />
+					</Routes>
+				</DataProvider>
+			</div>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 }
--- ui/src/pages/Clients/index.tsx
+++ ui/src/pages/Clients/index.tsx
@@ -1,30 +1,31 @@
-import { memo, useContext, useEffect } from "react";
-import { Paper, Typography } from "@mui/material";
-import { StateContext } from "../../store/DataProvider";
-import Page from "../../components/Page";
-import ClientTable from "./ClientTable";
-import { getClients } from "../../services/api";
+import { memo, useContext, useEffect } from 'react';
+import { Paper, Typography } from '@mui/material';
+import { StateContext } from '../../store/DataProvider';
+import Page from '../../components/Page';
+import ClientTable from './ClientTable';
+import { getClients } from '../../services/api';
+
+import { useQuery } from '@tanstack/react-query';
 
 function Clients() {
-  const { state, dispatch } = useContext(StateContext);
-  const { clients } = state;
+	const {
+		isPending,
+		error,
+		data: clients,
+		isFetching,
+	} = useQuery({
+		queryKey: ['clients'],
+		queryFn: () => getClients().then((clients) => clients),
+	});
 
-  useEffect(() => {
-    getClients().then((clients) =>
-      dispatch({ type: "FETCH_ALL_CLIENTS", data: clients })
-    );
-  }, [dispatch]);
-
-  return (
-    <Page>
-      <Typography variant="h4" sx={{ textAlign: "start" }}>
-        Clients
-      </Typography>
-      <Paper sx={{ margin: "auto", marginTop: 3 }}>
-        <ClientTable clients={clients} />
-      </Paper>
-    </Page>
-  );
+	return (
+		<Page>
+			<Typography variant='h4' sx={{ textAlign: 'start' }}>
+				Clients
+			</Typography>
+			<Paper sx={{ margin: 'auto', marginTop: 3 }}>{clients && <ClientTable clients={clients} />}</Paper>
+		</Page>
+	);
 }
 
 export default memo(Clients);
